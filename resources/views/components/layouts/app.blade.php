ddaceb7e89922b515485631adb3a04b5b270eb09
PK <p.khazaei@snappgrocery.com>
2025-09-30 11:20:25 +0330

    fix: adjust font paths in CSS and refine environment check for asset loading in layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,10 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ $title ?? 'جمه‌شاپ' }}</title>
-        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @if (file_exists(public_path('build/manifest.json')) || (app()->environment('local') && file_exists(public_path('hot'))))
             @vite(['resources/css/app.css','resources/js/app.js'])
-        @else
-            <script src="https://cdn.tailwindcss.com"></script>
         @endif
         
         <style>
