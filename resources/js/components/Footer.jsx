110edb303b69a3a929c3713c3b5b7406fa35747e
PK <p.khazaei@snappgrocery.com>
2025-11-18 20:18:14 +0330

    enamad

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -12,11 +12,11 @@ function Footer() {
                             <img src="/images/logo.png" alt="Logo" className="h-8 w-8"/>
                             <span className="font-bold text-xl text-white">جمه</span>
                             {/* E-Namad logo */}
-                            <a referrerPolicy='origin' target='_blank'
-                               href='https://trustseal.enamad.ir/?id=674581&Code=ET4La7gkVYrzYQ0tyceMQvKvcRCjJd3G'><img
-                                referrerPolicy='origin'
-                                src='https://trustseal.enamad.ir/logo.aspx?id=674581&Code=ET4La7gkVYrzYQ0tyceMQvKvcRCjJd3G'
-                                alt='' style='cursor:pointer' code='ET4La7gkVYrzYQ0tyceMQvKvcRCjJd3G'/></a>
+                            <a referrerPolicy="origin" target="_blank"
+                               href="https://trustseal.enamad.ir/?id=674581&Code=ET4La7gkVYrzYQ0tyceMQvKvcRCjJd3G"><img
+                                referrerPolicy="origin"
+                                src="https://trustseal.enamad.ir/logo.aspx?id=674581&Code=ET4La7gkVYrzYQ0tyceMQvKvcRCjJd3G"
+                                alt="" style={{cursor: 'pointer'}} code="ET4La7gkVYrzYQ0tyceMQvKvcRCjJd3G"/></a>
                         </div>
                         <p className="text-gray-400 mb-4">
                             فروشگاه آنلاین لباس با کیفیت و استایل مینیمال
